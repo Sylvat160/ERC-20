579017bacd6c333657e0acef3741b4b137209772
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-02-10 13:48:01 +0000

    (add): hardhat config file refactoring

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
+// require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+      {
+        version: "0.4.24",
+      },
+    ],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId:31337
+    },
+    localhost: {
+      chainId:31337
+    }
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1:0
+    }
+  }
 };
