0851f6f1e5a4e5dbcccb88b9b3bb42de5851f82b
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-02-11 13:33:38 +0000

    (fix) deploying error

--- deploy/01-deploy.js
+++ deploy/01-deploy.js
@@ -8,16 +8,18 @@ const {
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
-  const contract = 
-//   const ourToken = await deploy("OurToken", {
-//     from: deployer,
-//     args: [INITIAL_SUPPLY],
-//     log: true,
-//     // we need to wait if on a live network so we can verify properly
-//     waitConfirmations: network.config.blockConfirmations || 1,
-//   })
+//   const contract = 
+
+
+  const ourToken = await deploy("Token", {
+    from: deployer,
+    args: [INITIAL_SUPPLY],
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    waitConfirmations: network.config.blockConfirmations || 1,
+  })
   log(`ourToken deployed at`)
-  log()
+  log(ourToken.address)
 
 //   if (
 //     !developmentChains.includes(network.name) &&
